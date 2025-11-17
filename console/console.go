9407e3b7fd587625e2fac9c6daecceab35194f42
Malikov Egor <nepsteren@mail.ru>
2025-11-18 00:44:16 +0500

    add command add, work with json file

--- console/console.go
+++ console/console.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	task "taskTracker/Task"
 )
 
 func meeting() {
@@ -18,40 +19,56 @@ func help() {
 	fmt.Println("command 'delete' \"1\" - delete task")
 	fmt.Println("command 'mark-in-progress' \"1\" - mark task in progress")
 	fmt.Println("command 'mark-done' \"1\" - mark task done")
-	fmt.Println("command 'list done' - list done task")
-	fmt.Println("command 'list todo' - list todo task")
-	fmt.Println("command 'list in-progress' - list task in progress")
+	fmt.Println("command 'list' - list all tasks")
+	fmt.Println("command 'list done' - list done tasks")
+	fmt.Println("command 'list todo' - list todo tasks")
+	fmt.Println("command 'list in-progress' - list tasks in progress")
 	fmt.Println()
+}
+
+func add(description string) {
+	task.CreateTask(description)
 }
 
 func swithCommand(command string) {
 	commands := strings.Fields(command)
 	command = commands[0]
+
 	switch command {
 	case "help":
 		help()
-	case "add":
-		fmt.Println("some add")
+	case "list":
+		fmt.Println("some list")
 		fmt.Println()
+	case "add":
+		description := commands[1]
+		add(description)
 	case "delete":
 		fmt.Println("some delete")
 		fmt.Println()
 	case "update":
-		fmt.Println("update task")
+		fmt.Println("update something")
+		fmt.Println()
+	case "mark-in-progress":
+		fmt.Println("some delete")
+		fmt.Println()
+	case "mark-done":
+		fmt.Println("some delete")
 		fmt.Println()
 	case "list done":
 		fmt.Println("some done list")
 		fmt.Println()
-	case "list not done":
+	case "list todo":
 		fmt.Println("some not done list")
 		fmt.Println()
-	case "list in progress":
+	case "list in-progress":
 		fmt.Println("some progress list")
 		fmt.Println()
 	default:
 		fmt.Println("wrong input, try command \"help\"")
 		fmt.Println()
 	}
+
 }
 
 func usetInput() {
--- task/task.go
+++ task/task.go
@@ -1,20 +1,63 @@
 package task
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+var id = 1
+
 type Task struct {
-	id          int
-	description string
-	status      string //todo, in-progress, done
-	createdAt   string
-	updatedAt   string
+	Id          int    `json:"id"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
 }
 
-func CreateTask() Task {
-	firstTask := Task{
-		id:          1,
-		description: "to do something",
-		status:      "todo",
-		createdAt:   "0000.00.00",
-		updatedAt:   "0000.00.00",
+func loadTasks() []Task {
+	data, err := os.ReadFile("tasks.json")
+	if err != nil {
+		log.Fatal(err)
 	}
-	return firstTask
+
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	return tasks
+}
+
+func CreateTask(description string) {
+	_, err := os.Stat("tasks.json")
+	if os.IsNotExist(err) {
+		os.Create("tasks.json")
+		id = 0
+	}
+
+	tasks := loadTasks()
+	id = len(tasks) + 1
+
+	task := Task{Id: id,
+		Description: description,
+		Status:      "todo",
+		CreatedAt:   time.Now().Format("2006-01-02 15:04:05"),
+		UpdatedAt:   "",
+	}
+
+	tasks = append(tasks, task)
+
+	_, err = json.Marshal(tasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.WriteFile("tasks.json", value, 0644)
+	fmt.Println(tasks)
+
 }
